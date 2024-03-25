b5f862b7b25e1f21feff79e64531c42796961951
vortep.7 <0ndreyp7777@gmail.com>
2024-03-25 16:42:10 +0300

    Initial Commit

---
+++ ServiceHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ServiceHub
+//
+//  Created by Андрей Петров on 25.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
