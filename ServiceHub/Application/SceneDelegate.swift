b682612c85a2293d1ac9b585323b5b865fd17979
vortep.7 <0ndreyp7777@gmail.com>
2024-03-25 16:45:48 +0300

    add .gitignore

---
+++ ServiceHub/Application/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  ServiceHub
+//
+//  Created by Андрей Петров on 25.03.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
